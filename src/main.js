08f9c2fe77886843eb38b8e41bd20f2925b95e60
João Guilherme <joaaogui@gmail.com>
2023-05-02 21:31:50 -0300

    fix: adds fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { inject } from "@vercel/analytics";
+inject();
+createApp(App).mount("#app");
